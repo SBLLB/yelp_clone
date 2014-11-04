a3edb356d04f1458fe5e4f0d6ab827aa04bb7343
RachelNolan <mail@rachelnolan.com>
2014-11-04 19:19:42 +0000

    Added User ID to reviews schema

---
+++ spec/features/reviewing_restaurant_features_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+context 'when reviewing a restaurant' do 
+
+		it 'a user can leave a review' do 
+
+		end
+
+		it 'a user can only leave one review per restaurant' do 
+
+		end
+
+end
+
+context 'when deleting a review od a restaurant' do 
+
+		it 'a user can only delete a review they authored' do 
+
+		end
+
+end
--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -5,4 +5,12 @@ RSpec.describe Review, :type => :model do
 		review = Review.new(rating: 10)
 		expect(review).to have(1).error_on(:rating)
 	end 
+
+	it 'has records the user ID of the user that authored it' do 
+		review = Review.new(rating: 10, thoughts: "Blah Blah", user_id: 1)
+
+	end 
+
 end
+
+# Do I have to test that a review has a 'author' here?
