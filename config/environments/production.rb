ab5e45108e435a24aed44419f165a10e04b552a1
RachelNolan <mail@rachelnolan.com>
2014-11-04 11:09:21 +0000

    Added Devise gem for user login/authentication. Added sign in, up & out to root

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,7 @@
 Rails.application.configure do
+  # Added when installed devise gem
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
