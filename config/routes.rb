47949cd5fbeb3af6d3b1caa6fed29cd8bdad2304
RachelNolan <mail@rachelnolan.com>
2014-11-04 12:41:03 +0000

    Added login by facebook

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
 
-  devise_for :users
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   resources :restaurants do
     resources :reviews
   end
+
+devise_scope :user do
+  get 'sign_out', :to => 'devise/sessions#destroy', :only => :destroy_user_session_path
+end
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
